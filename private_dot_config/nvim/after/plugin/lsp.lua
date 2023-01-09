cd97bc86d3db254b5a2fb6ec015a42c0e0f9767d
Vincent Berthier <vincent.berthier@posteo.org>
2023-01-09 22:44:12 +0100

    Update .config/nvim/after/plugin/lsp.lua

--- private_dot_config/nvim/after/plugin/lsp.lua
+++ private_dot_config/nvim/after/plugin/lsp.lua
@@ -195,39 +195,3 @@ vim.diagnostic.config({
     virtual_text = true,
 })
 
-local status, rust = pcall(require, "rust-tools")
-if not status then
-    vim.notify("Could not load rust-tools in LSP")
-    return
-end
-rust.setup({
-    tools = {
-        runnables = {
-            use_telescope = true,
-        },
-        inlay_hints = {
-            auto = true,
-            show_parameter_hints = false,
-            parameter_hints_prefix = "",
-            other_hints_prefix = "",
-        },
-    },
-
-    -- all the opts to send to nvim-lspconfig
-    -- these override the defaults set by rust-tools.nvim
-    -- see https://github.com/neovim/nvim-lspconfig/blob/master/CONFIG.md#rust_analyzer
-    server = {
-        -- on_attach is a callback called when the language server attachs to the buffer
-        on_attach = on_attach,
-        settings = {
-            -- to enable rust-analyzer settings visit:
-            -- https://github.com/rust-analyzer/rust-analyzer/blob/master/docs/user/generated_config.adoc
-            ["rust-analyzer"] = {
-                -- enable clippy on save
-                checkOnSave = {
-                    command = "clippy",
-                },
-            },
-        },
-    },
-})
