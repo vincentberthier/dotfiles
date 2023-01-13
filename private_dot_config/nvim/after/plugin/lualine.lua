009347895bd79d520c1ccf8ab30184c869857ca5
Vincent Berthier <vincent.berthier@posteo.org>
2023-01-13 14:13:54 +0100

    Update .config/nvim/after/plugin/lualine.lua

--- private_dot_config/nvim/after/plugin/lualine.lua
+++ private_dot_config/nvim/after/plugin/lualine.lua
@@ -4,10 +4,16 @@ if not status then
     return
 end
 
+local function get_spaces()
+    return "" .. vim.api.nvim_buf_get_option(0, "shiftwidth") .. " spaces"
+end
+
 lualine.setup {
     options = {
         icons_enabled = true,
         theme = "iceberg_dark",
-
-    }
+        sections = {
+            lualine_x = {'encoding', 'fileformat', 'filetype'}
+        }
+    },
 }
