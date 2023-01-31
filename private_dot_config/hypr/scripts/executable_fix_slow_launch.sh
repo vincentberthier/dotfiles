2488c5e178b13da634698cea0cd19e84c93a494c
Vincent Berthier <vincent.berthier@posteo.org>
2023-01-31 19:27:20 +0100

    Update .config/hypr/hyprland.conf Update .config/hypr/scripts/fix_slow_launch.sh Add .config/hypr/scripts/kill_window.sh Add .config/paru/paru.conf

--- private_dot_config/hypr/scripts/executable_fix_slow_launch.sh
+++ private_dot_config/hypr/scripts/executable_fix_slow_launch.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-sleep 4
+sleep 1
+killall xdg-desktop-portal-wlr-hyprland
 killall xdg-desktop-portal-wlr
 killall xdg-desktop-portal
-/usr/lib/xdg-desktop-portal-wlr &
-sleep 4
+/usr/libexec/xdg-desktop-portal-wlr-hyprland &
+sleep 2
 /usr/lib/xdg-desktop-portal &
