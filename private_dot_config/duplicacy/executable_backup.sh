90badddf744c60fed014efcb49862509d26e7c5c
Vincent Berthier <vincent.berthier@posteo.org>
2023-02-02 21:36:59 +0100

    Add .config/duplicacy/ConfigFiles/.duplicacy/cache/Aegis/chunks/.keep Add .config/duplicacy/ConfigFiles/.duplicacy/cache/Aegis/snapshots/Code/.keep Add .config/duplicacy/ConfigFiles/.duplicacy/cache/Aegis/snapshots/ConfigFiles/.keep Add .config/duplicacy/ConfigFiles/.duplicacy/cache/Aegis/snapshots/Documents/.keep Add .config/duplicacy/ConfigFiles/.duplicacy/cache/Aegis/snapshots/Images/.keep Add .config/duplicacy/ConfigFiles/.duplicacy/preferences Add .config/duplicacy/Documents/.duplicacy/cache/Aegis/chunks/.keep Add .config/duplicacy/Documents/.duplicacy/cache/Aegis/snapshots/Documents/.keep Add .config/duplicacy/Documents/.duplicacy/preferences Add .config/duplicacy/Images/.duplicacy/cache/Aegis/chunks/.keep Add .config/duplicacy/Images/.duplicacy/cache/Aegis/snapshots/Code/.keep Add .config/duplicacy/Images/.duplicacy/cache/Aegis/snapshots/ConfigFiles/.keep Add .config/duplicacy/Images/.duplicacy/cache/Aegis/snapshots/Documents/.keep Add .config/duplicacy/Images/.duplicacy/cache/Aegis/snapshots/Images/.keep Add .config/duplicacy/Images/.duplicacy/preferences Add .config/duplicacy/code/.duplicacy/cache/Aegis/chunks/.keep Add .config/duplicacy/code/.duplicacy/cache/Aegis/snapshots/Code/.keep Add .config/duplicacy/code/.duplicacy/preferences Add .config/duplicacy/backup.sh Add .config/duplicacy/filters.txt Add .config/duplicacy/preferences

---
+++ private_dot_config/duplicacy/executable_backup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Cron job to backup locally
+# To init a new repository: duplicacy init --repository FOLDER_TO_BACKUP --storage-name Aegis BACKUP_NAME /mnt/aegis/backup/
+
+LOG_PATH="$XDG_DATA_HOME/duplicacy/"
+mkdir -p "$LOG_PATH"
+
+# Remove log files older than a weep
+find "$LOG_PATH" -type f -mtime 6 -delete
+
+# Create the new log file
+LOG_PATH=$XDG_DATA_HOME/duplicacy/$(date +%Y-%m-%d).log
+
+echo "-------------------------------------------------------" >> "$LOG_PATH"
+echo "Début de la sauvegarde: $(date)" >> "$LOG_PATH"
+echo "-------------------------------------------------------" >> "$LOG_PATH"
+
+ROOT=$PWD
+for dir in */; do
+    echo "------------- Traitement de $dir -------------" >> "$LOG_PATH"
+    cd "$ROOT/$dir"
+    /usr/bin/duplicacy backup -threads 4 -stats -dry-run >> "$LOG_PATH"
+done
+echo "------------- Vérification d’intégrité -------------" >> "$LOG_PATH"
+/usr/bin/duplicacy check -stats -tabular >> "$LOG_PATH"
