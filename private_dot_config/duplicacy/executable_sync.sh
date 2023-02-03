8cf110725205f68b6e5e873d781ffcca53860d01
Vincent Berthier <vincent.berthier@posteo.org>
2023-02-03 17:09:45 +0100

    Update .config/duplicacy/sync.sh

--- private_dot_config/duplicacy/executable_sync.sh
+++ private_dot_config/duplicacy/executable_sync.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 # Prune old snapshots
-/usr/bin/duplicacy prune -keep 1:7       # 1 revision per day after a week
-/usr/bin/duplicacy prune -keep 7:30      # 1 revision per week after a month
-/usr/bin/duplicacy prune -keep 30:180    # 1 revision per month after six months
-# /usr/bin/duplicacy prune -keep 0:360     # Discard everything after a year
+# 1 revision per day for a week, then 1 per week for a month then 1 per month after six months
+/usr/bin/duplicacy prune -threads 8 -keep 30:180 -keep 7:30 -keep 1:7
+# /usr/bin/duplicacy prune -threads 4 -keep 0:360     # Discard everything after a year
 
 
 LOG_PATH="/home/vincent/.local/share/duplicacy/$(date +%Y-%m-%d)-sync.log"
