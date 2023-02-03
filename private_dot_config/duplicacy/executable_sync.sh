1bb48f4eba9fb887f55207f8c807aa603e9a4730
Vincent Berthier <vincent.berthier@posteo.org>
2023-02-03 16:04:01 +0100

    Update .config/duplicacy/sync.sh

--- private_dot_config/duplicacy/executable_sync.sh
+++ private_dot_config/duplicacy/executable_sync.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+# Prune old snapshots
+/usr/bin/duplicacy prune -keep 1:30
+
 LOG_PATH="/home/vincent/.local/share/duplicacy/$(date +%Y-%m-%d)-sync.log"
 /usr/bin/rclone sync --progress /mnt/aegis/backup/ crypt:backup >> "$LOG_PATH"
