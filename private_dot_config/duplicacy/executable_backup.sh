facf7ca07110664101a3ec8bd630a27f6b7cb05a
Vincent Berthier <vincent.berthier@posteo.org>
2023-02-03 02:40:14 +0100

    Update .config/duplicacy/backup.sh

--- private_dot_config/duplicacy/executable_backup.sh
+++ private_dot_config/duplicacy/executable_backup.sh
@@ -3,7 +3,7 @@
 # Cron job to backup locally
 # To init a new repository: duplicacy init --repository FOLDER_TO_BACKUP --storage-name Aegis BACKUP_NAME /mnt/aegis/backup/
 
-LOG_PATH="/home/vincent/.local/share/duplicacy/"
+LOG_PATH="/home/vincent/.local/share/duplicacy"
 mkdir -p "$LOG_PATH"
 
 # Remove log files older than a weep
@@ -12,12 +12,14 @@ find "$LOG_PATH" -type f -mtime 6 -delete
 # Create the new log file
 LOG_FILE=$LOG_PATH/$(date +%Y-%m-%d).log
 
+echo "$LOG_FILE"
+exit
 echo "-------------------------------------------------------" >> "$LOG_FILE"
 echo "Début de la sauvegarde: $(date)" >> "$LOG_FILE"
 echo "-------------------------------------------------------" >> "$LOG_FILE"
 
 REPO_ROOT=/home/vincent/.config/duplicacy/
-for dir in */; do
+for dir in "$REPO_ROOT"/*/; do
     echo "------------- Traitement de $dir -------------" >> "$LOG_FILE"
     cd "$REPO_ROOT/$dir"
     /usr/bin/duplicacy backup -threads 4 -stats >> "$LOG_FILE"
