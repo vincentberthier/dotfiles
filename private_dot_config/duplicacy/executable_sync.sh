5cbf5427f2004fecc9db05a9003946192624f009
Vincent Berthier <vincent.berthier@posteo.org>
2023-02-21 23:07:21 +0100

    Update .config/duplicacy/sync.sh Update .config/nvim/lua/vincent/bepo.lua

--- private_dot_config/duplicacy/executable_sync.sh
+++ private_dot_config/duplicacy/executable_sync.sh
@@ -2,7 +2,8 @@
 
 # Prune old snapshots
 # 1 revision per day for a week, then 1 per week for a month then 1 per month after six months
-/usr/bin/duplicacy prune -threads 8 -keep 30:180 -keep 7:30 -keep 1:7
+cd /home/vincent/.config/duplicacy/images/
+/usr/bin/duplicacy prune -threads 8 -keep 30:180 -keep 7:30 -keep 1:7 -all -exhaustive
 # /usr/bin/duplicacy prune -threads 4 -keep 0:360     # Discard everything after a year
 
 
