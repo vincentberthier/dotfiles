5cea77427d64505ebced9c3b302147e6ed0f042f
Vincent Berthier <vincent.berthier@posteo.org>
2023-07-09 13:45:29 +0200

    Remove .config/nvim/after/plugin/flash.lua Add .config/nvim/after/plugin/flash.lua

---
+++ private_dot_config/nvim/after/plugin/flash.lua
@@ -0,0 +1,75 @@
+local status_wk, wk = pcall(require, "which-key")
+if not status_wk then
+	print("Could not load which-key: default keymaps")
+	return
+end
+
+local keys = {
+	["<leader>"] = {
+		f = {
+			s = {
+				function()
+					require("flash").jump({
+						search = {
+							mode = function(str)
+								return "\\<" .. str
+							end,
+						},
+					})
+				end,
+				"Flash ",
+			},
+			S = {
+				function()
+					require("flash").treesitter()
+				end,
+				"Flash Treesitter",
+			},
+		},
+	},
+}
+
+wk.register(keys, { mode = "n" })
+wk.register(keys, { mode = "x" })
+wk.register(keys, { mode = "o" })
+
+wk.register({
+	["<leader>"] = {
+		f = {
+			r = {
+				function()
+					require("flash").remote()
+				end,
+				"Remote Flash",
+			},
+			R = {
+				function()
+					require("flash").treesitter_search()
+				end,
+				"Flash Treesitter Search",
+			},
+		},
+	},
+}, { mode = "o" })
+
+wk.register({
+	["<leader>"] = {
+		f = {
+			R = {
+				function()
+					require("flash").treesitter_search()
+				end,
+				"Flash Treesitter Search",
+			},
+		},
+	},
+}, { mode = "x" })
+
+wk.register({
+	["<C-s>"] = {
+		function()
+			require("flash").toggle()
+		end,
+		"Flash Treesitter Search",
+	},
+}, { mode = "c" })
