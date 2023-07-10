b4853298109bce3a6332d8bceff8c9f28db19971
Vincent Berthier <vincent.berthier@posteo.org>
2023-07-10 20:17:48 +0200

    Update .config/fish/conf.d/home.fish

--- private_dot_config/private_fish/conf.d/home.fish
+++ private_dot_config/private_fish/conf.d/home.fish
@@ -1,3 +1,32 @@
+# Default applications
+set -Ux EDITOR "nvim"
+set -Ux TERMINAL "foot"
+set -Ux BROWSER "firefox"
+set -Ux SHELL "fish"
+
+# XDG config paths
+set -Ux XDG_CONFIG_HOME "$HOME/.config"
+set -Ux XDG_CACHE_HOME "$HOME/.cache"
+set -Ux XDG_DATA_HOME "$HOME/.local/share"
+
+# Locales
+set -Ux LC_ALL "fr_FR.UTF-8"
+
+# Add some folders into the path
+fish_add_path $HOME/bin
+fish_add_path $HOME/.local/bin
+
+if [ -f "$HOME/.cargo/env" ]
+    fish_add_path "$HOME/cargo/env"
+end
+
+# SSH Agent
+if status is-login
+    and status is-interactive
+    set -Ua SSH_KEYS_TO_AUTOLOAD $HOME/.ssh/default
+    keychain --eval $SSH_KEYS_TO_AUTOLOAD | source
+end
+
 # For when keys break
 function archlinux-fix-keys
     doas pacman-key --init 
