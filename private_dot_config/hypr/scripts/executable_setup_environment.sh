9689461d93786cd92caf7430ae2924610c018072
Vincent Berthier <vincent.berthier@posteo.org>
2023-07-16 15:29:24 +0200

    Update .config/hypr/scripts/setup_environment.sh

--- private_dot_config/hypr/scripts/executable_setup_environment.sh
+++ private_dot_config/hypr/scripts/executable_setup_environment.sh
@@ -6,16 +6,6 @@ function move_firefox {
     hyprctl dispatch movetoworkspacesilent name:"$2",address:"$window"
 }
 
-function move_foot {
-    echo "$1"
-    hyprctl dispatch movetoworkspacesilent name:"$2",pid:"$1"
-}
-
-foots="$(hyprctl clients -j | jq -r '[.[] | select(.class=="foot")]' | jq -r '.[].pid')"
-readarray -t <<< "$foots"
-move_foot "${MAPFILE[0]}" dev
-move_foot "${MAPFILE[1]}" dev
-move_foot "${MAPFILE[2]}" misc
 sleep 5
 move_firefox "General" general
 move_firefox "Dev’" webdev
