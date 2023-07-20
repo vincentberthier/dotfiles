f65199ef955aa7397a67faacd92747dd55c05cb9
Vincent Berthier <vincent.berthier@posteo.org>
2023-07-20 22:51:39 +0200

    Update .config/nvim/lua/vincent/file_associations.lua

--- private_dot_config/nvim/lua/vincent/file_associations.lua
+++ private_dot_config/nvim/lua/vincent/file_associations.lua
@@ -1,6 +1,14 @@
 -- add mxx files to c++
 vim.filetype.add({
 	extension = {
-		mxx = "cpp"
-	}
+		mxx = "cpp",
+	},
+})
+
+-- Only want two indent spaces for nix files
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "nix",
+	callback = function()
+		vim.opt_local.shiftwidth = 2
+	end,
 })
