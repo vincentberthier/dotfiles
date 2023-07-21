d5ebd961a139a7ab7cf89e757150437b3214c2c6
Vincent Berthier <vincent.berthier@posteo.org>
2023-07-21 16:43:57 +0200

    Update .config/hypr/scripts/setup_environment.sh

--- private_dot_config/hypr/scripts/executable_setup_environment.sh
+++ private_dot_config/hypr/scripts/executable_setup_environment.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/env bash
 set -e
 
-function move_libreWolf {
-    window=$(hyprctl clients -j | jq -r '[.[] | select((.title | startswith("'"$1"'")) and (.class=="LibreWolf"))]' | jq -r '.[].address')
-    hyprctl dispatch movetoworkspacesilent name:"$2",address:"$window"
+function move_librewolf {
+    readarray -t windows < <(hyprctl clients -j | jq -r '[.[] | select((.title | startswith("'"$1"'")) and (.class=="LibreWolf"))]' | jq -r '.[].address')
+    for w in "${windows[@]}"; do
+        hyprctl dispatch movetoworkspacesilent name:"$2",address:"$w"
+    done
 }
 
-sleep 5
+# sleep 5
 move_librewolf "General" general
-move_libreWolf "Dev’" webdev
+move_librewolf "Dev’" webdev
 move_librewolf "Work" work
 move_librewolf "New Window" misc
 
-sleep 10
-hyprctl dispatch movetoworkspacesilent name:chat,discord
+# sleep 10
+# hyprctl dispatch movetoworkspacesilent name:chat,discord
