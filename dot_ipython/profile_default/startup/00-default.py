08f4a447a2aecb90c58301878a706954f408419d
Vincent Berthier <vincent.berthier@posteo.org>
2023-07-26 23:53:47 +0200

    Update .ipython/profile_default/startup/00-default.py

--- dot_ipython/profile_default/startup/00-default.py
+++ dot_ipython/profile_default/startup/00-default.py
@@ -1,4 +1,19 @@
+#!env python3
+
+# File: .local/share/chezmoi/dot_ipython/profile_default/startup/00-default.py
+# Project: chezmoi
+# Creation date: mer. 26 juil. 2023 23:53:47
+# Author: Vincent Berthier <vincent.berthier@posteo.org>
+# -----
+# Last modified: mer. 26 juil. 2023 23:53:47
+# Modified By: Vincent Berthier
+# -----
+# Copyright © 2023 Vincent Berthier - All rights reserved
+
 import numpy as np
 import scipy as sc
 import matplotlib.pyplot as plt
 import seaborn as sns; sns.set()
+
+import matplotlib
+matplotlib.use("TKAgg")
