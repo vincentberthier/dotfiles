a7f36e93415d9cca39ae1a7f6e1c24a408f549cf
Vincent Berthier <vincent.berthier@posteo.org>
2023-07-26 18:41:22 +0200

    Update .config/hypr/scripts/setup_environment.sh

--- private_dot_config/hypr/scripts/executable_setup_environment.sh
+++ private_dot_config/hypr/scripts/executable_setup_environment.sh
@@ -4,10 +4,22 @@ set -e
 function move_librewolf {
     readarray -t windows < <(hyprctl clients -j | jq -r '[.[] | select((.title | startswith("'"$1"'")) and (.class=="LibreWolf"))]' | jq -r '.[].address')
     for w in "${windows[@]}"; do
+        echo "moving window $w"
         hyprctl dispatch movetoworkspacesilent name:"$2",address:"$w"
     done
 }
 
+function move_foot {
+    echo "$1"
+    hyprctl dispatch movetoworkspacesilent name:"$2",pid:"$1"
+}
+
+
+foots="$(hyprctl clients -j | jq -r '[.[] | select(.class=="foot")]' | jq -r '.[].pid')"
+readarray -t <<< "$foots"
+move_foot "${MAPFILE[0]}" dev
+move_foot "${MAPFILE[1]}" dev
+move_foot "${MAPFILE[2]}" misc
 # sleep 5
 move_librewolf "General" general
 move_librewolf "Dev’" webdev
