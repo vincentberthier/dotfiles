106c2375758bb3cdd2e0bc9fe031b1164fb4be39
Vincent Berthier <vincent.berthier@posteo.org>
2023-08-23 22:47:24 +0200

    Update .config/duplicacy/backup.sh

--- private_dot_config/duplicacy/executable_backup.sh
+++ private_dot_config/duplicacy/executable_backup.sh
@@ -1,27 +1,26 @@
 #!/usr/bin/env bash
 
 # Cron job to backup locally
-# To init a new repository: duplicacy init --repository FOLDER_TO_BACKUP --storage-name Aegis BACKUP_NAME /mnt/aegis/backup/
 
-LOG_PATH="/home/vincent/.local/share/duplicacy"
+FOLDERS=("$HOME/code" "$HOME/Documents" "$HOME/Images" "$XDG_CONFIG_HOME")
+LOG_PATH="${XDG_DATA_HOME}/duplicacy"
 mkdir -p "$LOG_PATH"
 
 # Remove log files older than a weep
 find "$LOG_PATH" -type f -mtime 6 -delete
 
 # Create the new log file
-LOG_FILE=$LOG_PATH/$(date +%Y-%m-%d).log
+LOG_FILE=$LOG_PATH/$(date +%Y-%m-%d).log.tmp
 
 echo "-------------------------------------------------------" >> "$LOG_FILE"
 echo "Début de la sauvegarde: $(date)" >> "$LOG_FILE"
 echo "-------------------------------------------------------" >> "$LOG_FILE"
 
-REPO_ROOT=/home/vincent/.config/duplicacy
-for dir in "$REPO_ROOT"/*/; do
+for dir in "${FOLDERS[@]}"; do
     echo "------------- Traitement de $dir -------------" >> "$LOG_FILE"
     cd "$dir"
     /usr/bin/duplicacy backup -threads 4 >> "$LOG_FILE"
 done
-CHECK_LOG="$LOG_PATH/$(date +%Y-%m-%d)-check.log"
+CHECK_LOG="$LOG_PATH/$(date +%Y-%m-%d)-check.log.tmp"
 echo "------------- Vérification d’intégrité -------------" >> "$CHECK_LOG"
 /usr/bin/duplicacy check -stats -tabular >> "$CHECK_LOG"
