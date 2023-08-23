89894f291fcca4904b71e9729654a484cada9689
Vincent Berthier <vincent.berthier@posteo.org>
2023-08-23 22:48:44 +0200

    Update .config/duplicacy/sync.sh

--- private_dot_config/duplicacy/executable_sync.sh
+++ private_dot_config/duplicacy/executable_sync.sh
@@ -2,10 +2,6 @@
 
 # Prune old snapshots
 # 1 revision per day for a week, then 1 per week for a month then 1 per month after six months
-cd /home/vincent/.config/duplicacy/images/
+cd "${HOME}/images"
 /usr/bin/duplicacy prune -threads 8 -keep 30:180 -keep 7:30 -keep 1:7 -all -exhaustive
 # /usr/bin/duplicacy prune -threads 4 -keep 0:360     # Discard everything after a year
-
-
-LOG_PATH="/home/vincent/.local/share/duplicacy/$(date +%Y-%m-%d)-sync.log"
-/usr/bin/rclone sync --progress /mnt/aegis/backup/ crypt:backup >> "$LOG_PATH"
