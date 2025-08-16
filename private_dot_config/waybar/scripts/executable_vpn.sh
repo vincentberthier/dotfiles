0c5efedb865a6bb916b6c384fd8a68d12e97084d
Vincent Berthier <vincent.berthier@posteo.org>
2025-08-16 16:58:24 +0200

    fix waybar network

--- private_dot_config/waybar/scripts/executable_vpn.sh
+++ private_dot_config/waybar/scripts/executable_vpn.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-RES=$(ping -c 1 desktop1.px.cls.fr > /dev/null 2>&1 ; echo $?)
+RES=$(ping -c 1 tyrex-gl01-dev.kub.local > /dev/null 2>&1 ; echo $?)
 if [[ $RES == 0 ]]; then
     echo "󱫋"
 fi
