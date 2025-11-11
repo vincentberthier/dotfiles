7a9775426d4a7cbda5a3289203e9a417d0871eac
Vincent Berthier <vincent.berthier@posteo.org>
2025-11-11 16:15:55 +0100

    Update .config/waybar/scripts/track_spotify.sh

--- private_dot_config/waybar/scripts/executable_track_spotify.sh
+++ private_dot_config/waybar/scripts/executable_track_spotify.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-TARGET="/tmp/spotify_currently_playing.txt"
+TARGET="/tmp/qobuz_currently_playing.txt"
 
 if [[ ! -f $TARGET ]]; then
     touch "$TARGET"
 fi
 
-STATUS=$(playerctl -p spotify status 2> /dev/null)
-TRACK=$(playerctl -p spotify metadata --format "[{{ artist }}] {{ album }} - {{ title }}")
+STATUS=$(playerctl -p hifirs status 2> /dev/null)
+TRACK=$(playerctl -p hifirs metadata --format "[{{ artist }}] {{ album }} - {{ title }}")
 RES=""
 
 if [[ $STATUS == "Playing" ]]; then
